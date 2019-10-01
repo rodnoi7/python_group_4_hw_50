9df3fcec33562a724d0730b1e356ae5f8b5ce80c
Kaiypbaek <sydykov.99@gmail.com>
2019-10-01 16:37:55 +0600

    "Добавил модель Комментарий"

--- webapp/blog/models.py
+++ webapp/blog/models.py
@@ -12,3 +12,13 @@ class Article(models.Model):
     def __str__(self):
         return '%s (%s) - %s' % (self.title, self.created_at, self.author)
 
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, related_name='comments', on_delete=models.CASCADE, verbose_name='Статья')
+    text = models.TextField(max_length=400, verbose_name='Комментарий')
+    author = models.CharField(max_length=40, null=True, blank=True, default='Аноним', verbose_name='Автор')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+    def __str__(self):
+        return self.text[:20]
